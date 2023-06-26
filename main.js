c482e740d52d86248439df5d0a31eda18a151184
Onelove05 <D.Onelove05@gmail.com>
2023-06-27 00:42:23 +0700

    week2

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-let input = document.querySelector(".form-search-input");
-let btn = document.querySelector(".btn-icon");
-let parent = document.querySelector(".form-search");
-
-btn.addEventListener("click", () => {
-    parent.classList.toggle("active");
-    input.focus();
-});
+$(document).ready(function(){
+    $('.slick').slick({
+      Infinity:true,
+      prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+      dots:true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+    });
+  });
+  $(document).ready(function(){
+    $('.section-body').slick({
+        infinite: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+  });
