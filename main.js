86d914f1c15f3dadadf6fbf35f60fc22160312ec
Onelove05 <D.Onelove05@gmail.com>
2023-06-27 14:36:01 +0700

    week2.4

--- main.js
+++ main.js
@@ -19,3 +19,22 @@ $(document).ready(function(){
         autoplaySpeed: 5000,
     });
   });
+
+  $('.view-full-size').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product-photo-thumbs'
+  });
+  $('.product-photo-thumbs').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.view-full-size',
+    dots: true,
+    focusOnSelect: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    autoplay:false,
+    Infinity:false,
+  });
