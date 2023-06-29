c995498efc5465033566ed2072b80bfd8461a566
Onelove05 <D.Onelove05@gmail.com>
2023-06-29 21:07:09 +0700

    week2.6

---
+++ linkbst/linkbst.js
@@ -0,0 +1,30 @@
+$('.view-full-size').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.product-photo-thumbs'
+  });
+  $('.product-photo-thumbs').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.view-full-size',
+    dots: true,
+    focusOnSelect: true,
+    prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    autoplay:false,
+    Infinity:false,
+  });
+
+  $(document).ready(function(){
+    $('.section-body').slick({
+        infinite: false,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+        nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+  });
